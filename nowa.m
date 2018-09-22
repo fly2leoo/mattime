b46fcab529e123eedc626005b12ecdda5975e0ec
Zhiyuan Zou <zouzhyuan@163.com>
2018-09-22 11:34:17 +0800

    init codes

--- nowa.m
+++ nowa.m
@@ -5,4 +5,3 @@ nowDate = clock;
 time = sprintf('%d-%d-%d %d:%d:%.1f', nowDate(1), nowDate(2), nowDate(3), nowDate(4), nowDate(5), nowDate(5));
 
 end
-
--- nowt.m
+++ nowt.m
@@ -5,4 +5,3 @@ nowDate = clock;
 time = sprintf('%d_%d_%d_%d_%d', nowDate(1), nowDate(2), nowDate(3), nowDate(4), nowDate(5));
 
 end
-
